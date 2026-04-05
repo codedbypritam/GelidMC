c93b1c58c398d592f0f15578413892abc20330f3
Pritam Shaw <pritam2009shaw@gmail.com>
2026-04-05 19:22:45 +0530

    Added basic server, registry.

---
+++ gelid-server/src/main.rs
@@ -0,0 +1,22 @@
+use serde::Deserialize;
+use std::fs;
+use crate::server::FelidServer;
+
+mod server;
+
+fn main() {
+    let config: Config = toml::from_str(&fs::read_to_string("config.toml").unwrap()).unwrap();
+
+    println!("Starting server at {}:{}", config.server_ip, config.port);
+
+    let network_profile = server::NetworkProfile::new(config.server_ip, config.port);
+    let server: FelidServer = FelidServer::new(network_profile);
+
+    
+}
+
+#[derive(Deserialize)]
+struct Config {
+    server_ip: String,
+    port: u16
+}
---
+++ gelid-server/src/server.rs
@@ -0,0 +1,24 @@
+pub struct FelidServer {
+    pub network_profile: NetworkProfile
+}
+
+impl FelidServer {
+    pub fn new(network_profile: NetworkProfile) -> Self {
+        Self { network_profile }
+    }
+}
+
+pub struct NetworkProfile {
+    address: String,
+    port: u16
+}
+
+impl NetworkProfile {
+    pub fn new(address: String, port: u16) -> Self {
+        Self { address, port }
+    }
+
+    pub fn get_socket_address(&self) -> String {
+        format!("{}:{}", self.address, self.port)
+    }
+}
